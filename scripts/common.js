f26b2d1dd567223924d252f041f5d46280a736e2
David Labra Gaona <davidlabragaona@gmail.com>
2024-05-17 05:03:11 -0300

    fixed issues

---
+++ scripts/common.js
@@ -0,0 +1,16 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+const darkButton = document.querySelector("#drkBtn");
+const main = document.querySelector("main");
+const card = document.querySelector(".card");
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+darkButton.addEventListener('click', () => {
+    main.classList.toggle("dark");
+    card.classList.toggle("dark");
+
+});
